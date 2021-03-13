2cfa73ffcc3ad5abe7b9df63daac0d71db465f3f
Andreas Motl <andreas@tunfish.org>
2021-03-13 21:58:24 +0100

    Fix tunfish-ca endpoints again

--- tunfish_ca/urls.py
+++ tunfish_ca/urls.py
@@ -4,6 +4,7 @@ from tunfish_ca import views
 
 
 urlpatterns = [
-    path('issuer/<hex:serial>.pem', views.GenericCAIssuersViewPEM.as_view(), name='issuer'),
-    path('pki/autosign', views.PKIAutoSign.as_view(), name='autosign'),
+    path('issuer/<hex:serial>.pem', views.GenericCAIssuersViewPEM.as_view(), name='issuer_by_serial'),
+    path('issuer/<str:ca_name>.pem', views.GenericCAIssuersViewPEM.as_view(), name='issuer_by_name'),
+    path('pki/<str:ca_name>/autosign', views.PKIAutoSign.as_view(), name='autosign'),
 ]
